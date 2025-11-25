ae5531f09d523779f689b6671b9f2fef1542fec6
chae2 <saintcharry22@gmail.com>
2025-11-25 21:18:10 +0900

    update: process_stress

--- preprocessing/process_stress.py
+++ preprocessing/process_stress.py
@@ -10,10 +10,10 @@ def get_wet_bulb_temp(ta, hm):
     term3 = np.arctan(hm - 1.67633)
     term4 = 0.00391838 * np.power(hm, 1.5) * np.arctan(0.023101 * hm)
 
-    Tw = term1 + term2 - term3 + term4 - 4.686035
-    return Tw
+    tw = term1 + term2 - term3 + term4 - 4.686035
+    return tw
 
-def calc_summer_feel(ta, hm):
+def calc_heat_stress(ta, hm):
     """
     여름철 체감온도 (기상청 공식)
     """
@@ -22,41 +22,46 @@ def calc_summer_feel(ta, hm):
     # 공식 적용
     feel = -0.2442 + (0.55399 * tw) + (0.45535 * ta) \
            - (0.0022 * tw**2) + (0.00278 * tw * ta) + 3.0
-    return feel
+    stress = feel-ta
 
-def calc_winter_wind_chill(ta, ws):
+    return stress.clip(lower=0)
+
+def calc_cold_stress(ta, ws):
     """
     겨울철 풍냉 체감온도 (북미/기상청 통합 공식)
     """
-    # 풍속이 1.3m/s 미만일 때의 예외처리는 일단 제외하고 공식 그대로 적용
-    # 필요하다면 np.where로 풍속 조건을 추가할 수 있음
+    v_kmh = ws * 3.6
 
     # 지수 연산(**0.16)
-    v_pow = np.power(ws, 0.16)
+    v_pow = np.power(v_kmh, 0.16)
 
-    wc = 13.12 + (0.6215 * ta) - (11.37 * v_pow) + (0.3965 * ta * v_pow)
+    feel = 13.12 + (0.6215 * ta) - (11.37 * v_pow) + (0.3965 * ta * v_pow)
+    stress = ta-feel
 
+    return stress.clip(lower=0)
 
-    return wc
-
-df = pd.read_csv("data/청주_열공급량_기상변수_corona_추가.csv")
+df = pd.read_csv("data/청주_열공급량_기상변수_corona_추가.csv", encoding='utf-8-sig')
 df['datetime'] = pd.to_datetime(df['datetime'])
 df['month'] = df['datetime'].dt.month
 
 is_summer = (df['month'] >= 5) & (df['month'] <= 9)
 is_winter = ~is_summer
-df['heat_stress'] = np.nan
-df['cold_stress'] = np.nan
+df['heat_stress'] = 0.0
+df['cold_stress'] = 0.0
 
 if is_summer.any():
-    df.loc[is_summer, 'heat_stress'] = calc_summer_feel(
+    df.loc[is_summer, 'heat_stress'] = calc_heat_stress(
         df.loc[is_summer, '기온(°C)'],
-        df.loc[is_summer, '풍속(m/s)']
+        df.loc[is_summer, '상대습도(%)']
     )
 if is_winter.any():
-    df.loc[is_winter, 'cold_stress'] = calc_winter_wind_chill(
+    winter_stress = calc_cold_stress(
         df.loc[is_winter, '기온(°C)'],
         df.loc[is_winter, '풍속(m/s)']
     )
-print(df.head())
+    df.loc[is_winter, 'cold_stress'] = winter_stress
+    invalid_condition = (df['기온(°C)'] >= 10) | (df['풍속(m/s)'] < 1.3)
+    df.loc[is_winter & invalid_condition, 'cold_stress'] = 0
+
+print(df[['datetime', '기온(°C)', '상대습도(%)', '풍속(m/s)', 'heat_stress', 'cold_stress']].head())
 df.to_csv('weather_data_with_feel.csv', index=False, encoding='utf-8-sig')
